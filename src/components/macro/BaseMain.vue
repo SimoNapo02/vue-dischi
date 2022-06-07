45ec738d4753efa934b99b6edbd2e7f5033a1750
SimoNapo02 <simonematteonapolitano@gmail.com>
2022-06-07 21:39:33 +0200

    esercizio finito + bootstrap (prima img non viene visualizzata)

---
+++ src/components/macro/BaseMain.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    <AlbumSection />
+  </main>
+</template>
+
+<script>
+import AlbumSection from "../sections/AlbumSec.vue";
+export default {
+  name: "BaseMain",
+  components: {
+    AlbumSection,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+main {
+  background-color: var(--primary-color);
+  min-height: calc(100vh - 3.75rem);
+}
+</style>
