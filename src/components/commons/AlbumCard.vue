5fac911840731db969bb3cf31524b5d2d96de914
SimoNapo02 <simonematteonapolitano@gmail.com>
2022-06-07 21:38:40 +0200

    esercizio completato + bootstrap (prima immagine non viene visualizzata)

---
+++ src/components/commons/AlbumCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="album">
+    <img class="album__img" :src="album.poster" :alt="album.title" />
+    <h5 class="album__title">{{ album.title }}</h5>
+    <span class="album__author">{{ album.author }}</span>
+    <small class="album__year">{{ album.year }}</small>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AlbumCard",
+  props: {
+    album: Object,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.album {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background-color: var(--secondary-color);
+  color: var(--secondary-color-text);
+  padding: 0.625rem;
+  text-align: center;
+  &__img {
+    width: 100%;
+    aspect-ratio: 1;
+  }
+  &__title {
+    text-transform: uppercase;
+    color: var(--primary-color-text);
+    margin: 0.625rem 0;
+  }
+  &__author {
+    display: block;
+  }
+}
+</style>
