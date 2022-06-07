2c558cf94499cc1c1f443664d949461021537419
SimoNapo02 <simonematteonapolitano@gmail.com>
2022-06-07 20:10:56 +0200

    reset totale esercizio + aggiunta header

---
+++ src/components/macro/BaseHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <div class="container">
+      <LogoSec />
+    </div>
+  </header>
+</template>
+
+<script>
+import LogoSec from "../sections/LogoSec.vue";
+export default {
+  name: "BaseHeader",
+  components: {
+    LogoSec,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  background-color: var(--secondary-color);
+  height: var(--header-md);
+}
+</style>
---
+++ src/components/sections/LogoSec.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="logo">
+    <img
+      class="logo__img img-fluid"
+      src="../../assets/img/spotify-logo.png"
+      alt="spotify logo"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LogoSec",
+};
+</script>
+
+<style lang="scss" scoped>
+.logo {
+  &__img {
+    height: 2.5rem;
+  }
+}
+</style>
